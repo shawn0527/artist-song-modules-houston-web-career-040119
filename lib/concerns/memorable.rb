56fcee94b600ed7845bfc66c72141ef858ef9bf4
Shawn Ren <shawnren527@gmail.com>
2019-04-21 03:54:54 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,9 @@
 module Memorable
-  module ClassInstances
-    def self.reset_all
-      self.all.clear
-    end
+  def self.reset_all
+    self.all.clear
+  end
 
-    def self.count
-      self.all.count
-    end
+  def self.count
+    self.all.count
   end
 end
