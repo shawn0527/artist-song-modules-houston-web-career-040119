00ab4c88d8e5b4f7bebfc682cb797eec557aee2c
Shawn Ren <shawnren527@gmail.com>
2019-04-21 04:10:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,9 +17,9 @@ class Artist
   #   @@artists.detect{|a| a.name == name}
   # end
   #
-  # def self.all
-  #   @@artists
-  # end
+  def self.all
+    @@artists
+  end
 
   # def self.reset_all
   #   self.all.clear
