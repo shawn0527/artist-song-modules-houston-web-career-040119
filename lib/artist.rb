342d19645803e23f1bbcd1544e9e212c55f1e986
Shawn Ren <shawnren527@gmail.com>
2019-04-21 04:07:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,13 +13,13 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@artists
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
+  # 
+  # def self.all
+  #   @@artists
+  # end
 
   # def self.reset_all
   #   self.all.clear
